d81de2b2d08d54586280bfdce7ba38a2177df03f
Matt Wittmann <mcwitt@gmail.com>
2020-10-25 17:13:39 -0700

    Initial commit

---
+++ default.nix
@@ -0,0 +1,41 @@
+{ sources ? import ./nix/sources.nix
+, haskellNix ? import sources."haskell.nix" { }
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003, compiler ? "ghc884" }:
+let
+  overlays = haskellNix.nixpkgsArgs.overlays ++ [
+    (self: super:
+      let inherit (super) lib;
+      in {
+        haskell-nix = super.haskell-nix // {
+          custom-tools = super.haskell-nix.custom-tools // {
+            haskell-language-server."0.5.1" = args:
+              (super.haskell-nix.cabalProject (args // {
+                name = "haskell-language-server";
+                src = super.fetchFromGitHub {
+                  owner = "haskell";
+                  repo = "haskell-language-server";
+                  rev = "e3fe0e7546aa91e44cc56cfe8ec078a026cf533a";
+                  sha256 =
+                    "17nzgpiacmrvwsy2fjx6a6pcpkncqcwfhaijvajm16jpdgni8mik";
+                  fetchSubmodules = true;
+                };
+                modules =
+                  [{ packages.haskell-language-server.doCheck = false; }];
+              })).haskell-language-server.components.exes.haskell-language-server;
+          };
+        };
+      })
+  ];
+
+  pkgs = import nixpkgsSrc {
+    inherit overlays;
+    inherit (haskellNix.nixpkgsArgs) config;
+  };
+
+in pkgs.haskell-nix.cabalProject {
+  compiler-nix-name = compiler;
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "learning";
+    src = ./.;
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,11 @@
+let hsPkgs = import ./default.nix { };
+in hsPkgs.shellFor {
+  withHoogle = true;
+  tools = {
+    cabal = "3.2.0.0";
+    haskell-language-server = "0.5.1";
+    hlint = "3.1.6";
+    ormolu = "0.1.2.0";
+    weeder = "2.1.0";
+  };
+}
