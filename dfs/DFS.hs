1ef193e139f22ebc82cab98a99e5b34c850415bd
Matt Wittmann <mcwitt@gmail.com>
2020-10-25 17:17:35 -0700

    DFS benchmarking

--- dfs/DFS.hs
+++ dfs/DFS.hs
@@ -1,30 +1,38 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module DFS where
+module Main where
 
-import Control.Monad.Primitive
-import Control.Monad.ST
+import Control.DeepSeq (NFData)
+import Control.Monad.Primitive (PrimMonad)
+import Control.Monad.ST (ST, runST)
 import Control.Monad.State
-import Data.Array
-  ( Array,
-    Ix,
-    array,
-    bounds,
-    (!),
+  ( MonadState (get),
+    State,
+    evalState,
+    modify,
+    replicateM,
   )
-import Data.STRef
+import Criterion.Main
+import Data.Functor ((<&>))
+import Data.STRef (STRef, modifySTRef, newSTRef, readSTRef)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import System.Random.MWC
-import Test.Hspec
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import GHC.Generics
+import System.Random.MWC.Probability
+import Test.Hspec (Expectation, shouldBe)
 
-type Vertex = Char
+type Vertex = Int
 
-type Table a = Array Vertex a
+type Table a = Vector a
 
 type Graph = Table [Vertex]
 
-data Tree a = Node a (Forest a) deriving (Eq, Show)
+data Tree a = Node a (Forest a) deriving (Eq, Show, Generic, NFData)
 
 type Forest a = [Tree a]
 
@@ -32,7 +40,7 @@ dfs :: Graph -> Vertex -> Tree Vertex
 dfs g = prune . generate g
 
 generate :: Graph -> Vertex -> Tree Vertex
-generate g u = let vs = g ! u in Node u (generate g <$> vs)
+generate g u = let vs = g V.! u in Node u (generate g <$> vs)
 
 prune :: Tree Vertex -> Tree Vertex
 prune = flip evalState Set.empty . clip
@@ -46,14 +54,12 @@ clip (Node u ns) = do
   return (Node u ns')
 
 g1 :: Graph
-g1 =
-  array
-    ('A', 'E')
-    [('A', ['B', 'C']), ('B', []), ('C', ['D', 'E']), ('D', []), ('E', ['A'])]
+g1 = V.fromList [[1, 2], [], [3, 4], [], [0]]
 
 test :: Expectation
 test = do
-  dfs g1 'A' `shouldBe` Node 'A' [Node 'B' [], Node 'C' [Node 'D' [], Node 'E' []]]
+  dfs g1 0
+    `shouldBe` Node 0 [Node 1 [], Node 2 [Node 3 [], Node 4 []]]
 
 pruneST :: Tree Vertex -> Tree Vertex
 pruneST t = runST $ newSTRef Set.empty >>= flip clipST t
@@ -69,12 +75,41 @@ clipST visitedRef (Node u ns) = do
 dfsST :: Graph -> Vertex -> Tree Vertex
 dfsST g = pruneST . generate g
 
--------------------------------------------------------------------------------
+type AdjacencyMatrix = Vector (Vector Bool)
 
-sample :: (PrimMonad m, Ix i, UniformRange i) => Int -> Array i a -> Gen (PrimState m) -> m [a]
-sample n xs g = fmap (xs !) <$> replicateM n (uniformRM (bounds xs) g)
+randomAdjacencyMatrix ::
+  PrimMonad m => Int -> Double -> Prob m AdjacencyMatrix
+randomAdjacencyMatrix n z = V.fromList <$> replicateM n row
+  where
+    row = V.fromList <$> replicateM n (bernoulli p)
+    p = z / fromIntegral n
 
-randomGraph :: PrimMonad m => [Vertex] -> Gen (PrimState m) -> m Graph
-randomGraph vs g = return $ array (minimum vs, maximum vs) []
-  -- where randomNeighbors = [sample n vs | n <-
+fromAdjacencyMatrix :: AdjacencyMatrix -> Graph
+fromAdjacencyMatrix mat = fmap findAll mat
+  where
+    findAll xs = [i | (i, x) <- zip [0 ..] (V.toList xs), x]
 
+randomGraph :: PrimMonad m => Int -> Double -> Prob m Graph
+randomGraph n = fmap fromAdjacencyMatrix . randomAdjacencyMatrix n
+
+runBench :: (Graph -> Vertex -> Tree Vertex) -> Int -> Double -> IO (Tree Vertex)
+runBench fun n z = create >>= sample (randomGraph n z) <&> \g -> fun g 0
+
+main :: IO ()
+main =
+  defaultMain
+    [ bgroup
+        "Using State"
+        [ bench "random graph with 100 vertices, z=10" $ nfIO (runBench dfs 100 10),
+          bench "random graph with 100 vertices, z=30" $ nfIO (runBench dfs 100 30),
+          bench "random graph with 300 vertices, z=10" $ nfIO (runBench dfs 300 10),
+          bench "random graph with 300 vertices, z=30" $ nfIO (runBench dfs 300 30)
+        ],
+      bgroup
+        "Using ST"
+        [ bench "random graph with 100 vertices, z=10" $ nfIO (runBench dfsST 100 10),
+          bench "random graph with 100 vertices, z=30" $ nfIO (runBench dfsST 100 30),
+          bench "random graph with 300 vertices, z=10" $ nfIO (runBench dfsST 300 10),
+          bench "random graph with 300 vertices, z=30" $ nfIO (runBench dfsST 300 30)
+        ]
+    ]
